0adf8c8724a9007880f11038b9042f30137b143f
Prashant Kumar Singh <mrprashantsingh06@gmail.com>
2026-09-23 23:45:28 +0530

    Add files via upload

---
+++ Lesson-01-Basics/01-hello-world.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("Prashant Singh\n");
+    return 0;
+}
---
+++ Lesson-01-Basics/02-variables-and-datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 23;
+    int weight = 60;
+    char name[] = "Prashant Singh";
+
+    printf("My name is %s\n", name);
+    printf("I am %d years old and weigh %d kg\n", age, weight);
+
+    return 0;
+}
